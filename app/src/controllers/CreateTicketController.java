be6bfbf84b5927d493ca737bbf81a13320192d36
Kevin <Kevin@Kevin-Laptop>
2017-04-21 21:43:52 -0500

    -createTicket functionality works -added values to the category, and priority combobox

--- app/src/controllers/CreateTicketController.java
+++ app/src/controllers/CreateTicketController.java
@@ -1,17 +1,24 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import models.CreateTicketModel;
 import objects.*;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.io.IOException;
 import java.text.DateFormat;
 
-public class CreateTicketController extends Dao {
+public class CreateTicketController{
 	@FXML
 	private Button logout;
 	@FXML
@@ -23,15 +30,17 @@ public class CreateTicketController extends Dao {
 	@FXML
 	private Button submit;
 	@FXML
-	private ChoiceBox priority;
+	private ComboBox<String> priority;
 	@FXML
-	private ChoiceBox category;
+	private ComboBox<String> category;
 
 	@FXML
 	public void createTicket(){
+		CreateTicketModel ctm = new CreateTicketModel();
+		
 		String newTitle = this.title.getText();
 		String newDescription = this.description.getText();
-		int newPriority = (int) this.priority.getValue();
+		int newPriority = Integer.parseInt(this.priority.getValue());
 		String newCategory = (String) this.category.getValue();
 		
 		Date date = new Date();
@@ -39,8 +48,27 @@ public class CreateTicketController extends Dao {
 		DateFormat ticketID = new SimpleDateFormat("yyyyMMddHHmmss");
 		
 		Tickets newTicket = new Tickets(ticketID.format(date), dateFormat.format(date), dateFormat.format(date), "Open", newTitle,
-				newDescription, "currentUser.getUsername()", newPriority, newCategory);
+				newDescription, "ktang", newPriority, newCategory);
 		
-		submitTicket(newTicket);
+		ctm.createTicket(newTicket);
+	}
+
+	@FXML
+	private void ButtonAction(ActionEvent event) throws IOException {
+		Stage stage;
+		Parent root;
+		if (event.getSource() == logout) {
+			// get reference to the button's stage
+			stage = (Stage) logout.getScene().getWindow();
+			// load up OTHER FXML document
+			root = FXMLLoader.load(getClass().getResource("/view/LoginView.fxml"));
+		} else {
+			stage = (Stage) back.getScene().getWindow();
+			root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
+		}
+		// create a new scene with root and set the stage
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 }
