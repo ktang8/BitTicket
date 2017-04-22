4d46a6576a7bd15eb4efebf3b3cb7effe752163f
Kevin <Kevin@Kevin-Laptop>
2017-04-22 09:10:15 -0500

    -startup page in create ticket for now

--- app/src/application/Main.java
+++ app/src/application/Main.java
@@ -1,8 +1,12 @@
 package application;
 	
+import controllers.CreateTicketController;
+import controllers.LoginController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -10,8 +14,25 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+//			FXMLLoader loader = new  FXMLLoader(getClass().getResource("/views/LoginView.fxml"));
+//			
+//			//Inflate the view using the loader
+//            AnchorPane root = (AnchorPane) loader.load();
+//			Scene scene = new Scene(root);
+//			
+//			//point to stylesheet for possible styles
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//            LoginController controller = loader.getController();
+//            
+//            //Set the parent stage in the controller
+//			primaryStage.show();
+			
+			
+			//load LoginView
+			FXMLLoader loader = new  FXMLLoader(getClass().getResource("/views/CreateTicketView.fxml"));
+            AnchorPane root = (AnchorPane) loader.load();
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
