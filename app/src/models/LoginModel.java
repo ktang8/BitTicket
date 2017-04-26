8d76192e069bce2afea0bb84a8e20355743983d7
kevin <kevin@104.194.105.133>
2017-04-26 14:45:47 -0500

    -added location sha1 hashing

--- app/src/models/LoginModel.java
+++ app/src/models/LoginModel.java
@@ -1,5 +1,9 @@
 package models;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -10,9 +14,16 @@ public class LoginModel extends Dao{
 		return getUser(username);
 	}
 	
-	public boolean verifyUser(String username, String password){
+	public boolean verifyUser(String username, String password) throws NoSuchAlgorithmException{
 		startConnection();
-		String query = "SELECT count(pid) AS verify FROM " + usersTable + " WHERE pass=SHA('" + password + "') and username='" + username + "'";
+		
+		//hashes the password before sending the hashed password to compare to the database
+		MessageDigest md = MessageDigest.getInstance("SHA-1");
+		md.update(password.getBytes());
+	    BigInteger hash = new BigInteger(1, md.digest());
+	    String hashStr = hash.toString(16);
+	    
+		String query = "SELECT count(pid) AS verify FROM " + usersTable + " WHERE pass='" + hashStr + "' and username='" + username + "'";
 		ResultSet rs;
 		try {
 			rs = stmt.executeQuery(query);
